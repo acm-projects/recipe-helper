e643362c16c711440a2e69fb98c2182b065eeeb3
DavidAOchoa <Od174140@gmail.com>
2019-02-21 13:22:06 -0600

    Testing and Learning, Updated a few things like logo and link.

--- recipe-helper-frontend/src/App.js
+++ recipe-helper-frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './salad.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,19 +7,23 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload. whats up yall
+            Recipe Helper WIP
           </p>
+          
           <a
             className="App-link"
-            href="https://reactjs.org"
+            href="https://www.acmutd.co/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn more about what we do at ACM!
           </a>
         </header>
+
+        <div>Icons made by <a href="https://www.flaticon.com/authors/monkik" title="monkik">monkik</a> from <a href="https://www.flaticon.com/" 			    title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" 			    title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+
       </div>
     );
   }
