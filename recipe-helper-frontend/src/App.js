d60b4c6bbf062052e0899ccc6f27b6b791c26c49
DavidAOchoa <Od174140@gmail.com>
2019-02-21 18:24:16 -0600

    Played around

--- recipe-helper-frontend/src/App.js
+++ recipe-helper-frontend/src/App.js
@@ -7,6 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
+        <i class="fas fa-bacon"></i>
         <img src={logo} className="App-logo" alt="logo" />
           <p>
             Recipe Helper WIP
