4836d2790d646fa588c0e956e973c20167c3384f
Mitali-Shenoy <mitsyblue@gmail.com>
2019-03-06 21:50:14 -0600

    removed nav bar

--- recipe-helper-frontend/src/App.js
+++ recipe-helper-frontend/src/App.js
@@ -42,42 +42,9 @@ return (
       </a>
    </header>
    <div>Icons made by <a href="https://www.flaticon.com/authors/monkik" title="monkik">monkik</a> from <a href="https://www.flaticon.com/" 			    title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" 			    title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
-   <SideNav
-      onSelect={(selected) =>
-      {
-      // Add your code here
-      }}
-      >
-      <SideNav.Toggle />
-      <SideNav.Nav defaultSelected="home">
-         <NavItem eventKey="home">
-            <NavIcon>
-               <i className="fa fa-fw fa-home" style={{ fontSize: '1.75em' }} />
-            </NavIcon>
-            <NavText>
-               Home
-            </NavText>
-         </NavItem>
-         <NavItem eventKey="charts">
-            <NavIcon>
-               <i className="fa fa-fw fa-line-chart" style={{ fontSize: '1.75em' }} />
-            </NavIcon>
-            <NavText>
-               Charts
-            </NavText>
-            <NavItem eventKey="charts/linechart">
-               <NavText>
-                  Line Chart
-               </NavText>
-            </NavItem>
-            <NavItem eventKey="charts/barchart">
-               <NavText>
-                  Bar Chart
-               </NavText>
-            </NavItem>
-         </NavItem>
-      </SideNav.Nav>
-   </SideNav>
+
+      {/* side nav */}
+
 </div>
 );
 }
