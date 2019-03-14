326ce3f9a130d4b2a8840e09d45549c5558fac4f
DavidAOchoa <Od174140@gmail.com>
2019-03-13 20:56:23 -0500

    Cleaned Up

--- recipe-helper-frontend/src/App.js
+++ recipe-helper-frontend/src/App.js
@@ -30,17 +30,18 @@ return (
       <i class="fas fa-bacon"></i>
       <img src={logo} className="App-logo" alt="logo" />
       <p>
-         Chef it! WIP
+         
       </p>
-      <a
-         className="App-link"
-         href="https://www.acmutd.co/"
-         target="_blank"
-         rel="noopener noreferrer"
-         >
-      Learn more about what we do at ACM!
+      <a>
+      {/* //    className="App-link"
+      //    href="https://www.acmutd.co/"
+      //    target="_blank"
+      //    rel="noopener noreferrer"
+      //    >
+      // Learn more about what we do at ACM! */}
       </a>
    </header>
+   
    <div>Icons made by <a href="https://www.flaticon.com/authors/monkik" title="monkik">monkik</a> from <a href="https://www.flaticon.com/" 			    title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" 			    title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
 
       {/* side nav */}
